599a03100fcbfcdf2f515f46b191c361d89fed3c
Shane <shane.seaton@gmail.com>
2017-01-23 15:45:07 +1100

    touch

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ S3_BUCKET_DEST=s3://dev.elsa.edu.au
 LOCAL_SITE_DIR=./site
 AWS_PROFILE=elsa
 
-echo "Uploading Site"
+echo "Uploading Site "
 aws s3 sync $LOCAL_SITE_DIR $S3_BUCKET_DEST --profile $AWS_PROFILE
