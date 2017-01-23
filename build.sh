6a56b88467e1750cfd5bbe0c0960b80353755b96
Shane <shane.seaton@gmail.com>
2017-01-23 15:36:41 +1100

    Added profile management for the aws to help different projects on the build server

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -eo pipefail
 
 S3_BUCKET_DEST=s3://dev.elsa.edu.au
 LOCAL_SITE_DIR=./site
+AWS_PROFILE=elsa
 
 echo "Uploading Site"
-aws s3 sync $LOCAL_SITE_DIR $S3_BUCKET_DEST
+aws s3 sync $LOCAL_SITE_DIR $S3_BUCKET_DEST --profile $AWS_PROFILE
