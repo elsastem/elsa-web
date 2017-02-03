b62aed1980a0d210ec5f3ce8ed09d5a065c00124
Shane <shane.seaton@gmail.com>
2017-02-03 15:11:28 +1100

    Update build process for EOI site

--- build.sh
+++ build.sh
@@ -3,8 +3,14 @@
 set -eo pipefail
 
 S3_BUCKET_DEST=s3://dev.elsa.edu.au
-LOCAL_SITE_DIR=./site
+LOCAL_SITE_DIR=./eoi-site/build
 AWS_PROFILE=elsa
+
+echo "Building Site"
+cd eoi-site
+npm install
+npm run gulp
+cd ..
 
 echo "Uploading Site "
 aws s3 sync $LOCAL_SITE_DIR $S3_BUCKET_DEST --profile $AWS_PROFILE
