a6d618d447bbe068aac4b9b727d81fe953123e4f
Shane <shane.seaton@gmail.com>
2017-02-10 22:52:45 +1100

    Added validation to form, and posting to AWS works

--- eoi-site/js/eoi.js
+++ eoi-site/js/eoi.js
@@ -4,120 +4,160 @@ var FormWizard = function () {
     return {
         init: function () {
             if (jQuery().bootstrapWizard) {
-                var r = $("#eoi_form"),
-                    t = $(".alert-danger", r),
-                    i = $(".alert-success", r);
-                r.validate({
+                var form = $("#eoi_form"),
+                    failPanel = $(".alert-danger", form),
+                    passPanel = $(".alert-success", form);
+                form.validate({
                     doNotHideMessage: true,
                     errorElement: "span",
                     errorClass: "help-block help-block-error",
                     focusInvalid: false,
                     rules: {
-                        username: {
-                            minlength: 5,
-                            required: true
-                        },
-                        password: {
-                            minlength: 5,
-                            required: true
-                        },
-                        rpassword: {
-                            minlength: 5,
+                        "preschool_name": "required",
+                        "preschool_address_line1": "required",
+                        //"preschool_address_line2": "required",
+                        "preschool_address_suburb": "required",
+                        "preschool_address_state": "required",
+                        "preschool_address_postcode": {
                             required: true,
-                            equalTo: "#submit_form_password"
-                        },
-                        fullname: {
-                            required: true
-                        },
-                        email: {
-                            required: true,
-                            email: true
-                        },
-                        phone: {
-                            required: true
-                        },
-                        gender: {
-                            required: true
-                        },
-                        address: {
-                            required: true
-                        },
-                        city: {
-                            required: true
-                        },
-                        country: {
-                            required: true
-                        },
-                        card_name: {
-                            required: true
-                        },
-                        card_number: {
-                            minlength: 16,
-                            maxlength: 16,
-                            required: true
-                        },
-                        card_cvc: {
                             digits: true,
-                            required: true,
-                            minlength: 3,
+                            minlength: 4,
                             maxlength: 4
                         },
-                        card_expiry_date: {
-                            required: true
-                        },
-                        "payment[]": {
+                        "preschool_category": "required",
+                        "preschool_service_number": {
                             required: true,
-                            minlength: 1
-                        }
+                            digits: true
+                        },
+                        "preschool_provider_name": "required",
+                        "preschool_provider_number": "required",
+                        "preschool_rating": "required",
+                        "preschool_type": "required",
+                        "wifi": "required",
+                        "prekindy": "required",
+                        "teacher": "required",
+                        "device": "required",
+                        "director_name": "required",
+                        "director_position": "required",
+                        "director_email": "required",
+                        "director_phone": "required",
+                        "primary_name": {
+                            required: "input[name=provide_primary]:checked"
+                        },
+                        "primary_position": {
+                            required: "input[name=provide_primary]:checked"
+                        },
+                        "primary_email": {
+                            required: "input[name=provide_primary]:checked",
+                            email: true
+                        },
+                        "primary_phone": {
+                            required: "input[name=provide_primary]:checked"
+                        },
+                        "secondary_name": {
+                            required: "input[name=provide_secondary]:checked"
+                        },
+                        "secondary_position": {
+                            required: "input[name=provide_secondary]:checked"
+                        },
+                        "secondary_email": {
+                            required: "input[name=provide_secondary]:checked",
+                            email: true
+                        },
+                        "secondary_phone": {
+                            required: "input[name=provide_secondary]:checked"
+                        },
+                        "other_discover": "required",
+                        "other_count": "required",
+                        // "other_atsi_owned",
+                        // "other_atsi_director",
+                        // "other_atsi_enrolled",
+                        // "other_atsi_engage",
+                        // "other_atsi_activities",
+                        "other_ella": "required",
+                        // "other_ella_2017": {
+                        //     required: "input[name=other_ella][value=yes]:checked"
+                        // },
+                        // "other_ella_2016": {
+                        //     required: "input[name=other_ella][value=yes]:checked"
+                        // },
+                        // "other_ella_2015": {
+                        //     required: "input[name=other_ella][value=yes]:checked"
+                        // },
+                        "declaration": "required"
                     },
-                    messages: {
-                        "payment[]": {
-                            required: "Please select at least one option",
-                            minlength: jQuery.validator.format("Please select at least one option")
-                        }
+                    errorPlacement: function (error, element) {
+                        var controlList = element.closest(".control-list");
+                        if(controlList.length > 0) {
+                            error.insertAfter(controlList[0]);
+                        } else 
+                            error.insertAfter(element);
                     },
-                    errorPlacement: function (e, r) {
-                        "gender" == r.attr("name") ? e.insertAfter("#form_gender_error") : "payment[]" == r.attr("name") ? e.insertAfter("#form_payment_error") : e.insertAfter(r)
+                    invalidHandler: function (event, validator) {
+                        passPanel.hide();
+                        failPanel.show();
+                        //App.scrollTo(t, -200)
                     },
-                    invalidHandler: function (e, r) {
-                        i.hide(), t.show()//, App.scrollTo(t, -200)
+                    highlight: function (element, errorClass) {
+                        $(element).closest(".form-group").removeClass("has-success").addClass("has-error");
                     },
-                    highlight: function (e) {
-                        $(e).closest(".form-group").removeClass("has-success").addClass("has-error")
+                    unhighlight: function (element, errorClass) {
+                        $(element).closest(".form-group").removeClass("has-error");
                     },
-                    unhighlight: function (e) {
-                        $(e).closest(".form-group").removeClass("has-error")
+                    success: function (label) {
+                        label.addClass("valid").closest(".form-group").removeClass("has-error").addClass("has-success");
                     },
-                    success: function (e) {
-                        "gender" == e.attr("for") || "payment[]" == e.attr("for") ? (e.closest(".form-group").removeClass("has-error").addClass("has-success"), e.remove()) : e.addClass("valid").closest(".form-group").removeClass("has-error").addClass("has-success")
-                    },
-                    submitHandler: function (e) {
-                        i.show(), t.hide(), e[0].submit()
+                    submitHandler: function (form) {
+                        passPanel.show();
+                        failPanel.hide();
+                        //form[0].submit();
+                        //console.log("Farts");
+                        var formData = $(form).serializeObject();
+                        var url = "https://ed5h9uai93.execute-api.ap-southeast-2.amazonaws.com/prod/eoi";// "https://api.elsa.edu.au/web/eoi";                    
+                        var apiKey = "huwyil4DTkGbPxduj8062871TorMtjM3CaSRS5Kh";
+                        
+                        $.ajax({
+                            url: url,
+                            method: "POST",
+                            contentType: "application/json",
+                            headers: {
+                                "x-api-key": apiKey
+                            },
+                            data: JSON.stringify(formData)
+                        }).done(function(result) {
+                            alert( "success" );
+                        })
+                        .fail(function( jqXHR, textStatus ) {
+                            alert( "error" );
+                        })
+                        .always(function() {
+                            alert( "complete" );
+                        });
                     }
                 });
-                var a = function () {
-                    $("#tab4 .form-control-static", r).each(function () {
-                        var e = $('[name="' + $(this).attr("data-display") + '"]', r);
-                        if (e.is(":radio") && (e = $('[name="' + $(this).attr("data-display") + '"]:checked', r)), e.is(":text") || e.is("textarea")) $(this).html(e.val());
-                        else if (e.is("select")) $(this).html(e.find("option:selected").text());
-                        else if (e.is(":radio") && e.is(":checked")) $(this).html(e.attr("data-title"));
-                        else if ("payment[]" == $(this).attr("data-display")) {
-                            var t = [];
-                            $('[name="payment[]"]:checked', r).each(function () {
-                                t.push($(this).attr("data-title"))
-                            }), $(this).html(t.join("<br>"))
-                        }
-                    })
-                };
-                var o = function (e, r, t) {
-                    var i = r.find("li").length,
-                        o = t + 1;
-                    $(".step-title", $("#eoi-form-wizard")).text("Step " + (t + 1) + " of " + i);
-                    jQuery("li", $("#eoi-form-wizard")).removeClass("done");
-                    for (var n = r.find("li"), s = 0; t > s; s++)
-                        jQuery(n[s]).addClass("done");
+                
+                var updateStep = function (tab, navigation, index) {
+                    var tabCount = navigation.find("li").length,
+                        current = index + 1;
+                    $(".step-title", $("#eoi-form-wizard")).text("Step " + (current) + " of " + tabCount);
+                    $("li", $("#eoi-form-wizard")).removeClass("done");
 
-                    1 == o ? $("#eoi-form-wizard").find(".button-previous").hide() : $("#eoi-form-wizard").find(".button-previous").show(), o >= i ? ($("#eoi-form-wizard").find(".button-next").hide(), $("#eoi-form-wizard").find(".button-submit").show(), a()) : ($("#eoi-form-wizard").find(".button-next").show(), $("#eoi-form-wizard").find(".button-submit").hide());
+                    for (var nav = navigation.find("li"), i = 0; index > i; i++) {
+                        $(nav[i]).addClass("done");
+                    }
+
+                    if(1 == current)
+                        $("#eoi-form-wizard").find(".button-previous").hide();
+                    else
+                        $("#eoi-form-wizard").find(".button-previous").show();
+
+                    if( current >= tabCount) {
+                        $("#eoi-form-wizard").find(".button-next").hide();
+                        $("#eoi-form-wizard").find(".button-submit").show();
+                    } else {
+                        $("#eoi-form-wizard").find(".button-next").show();
+                        $("#eoi-form-wizard").find(".button-submit").hide();
+                    }
 
                     // App.scrollTo($(".page-title"))
                 };
@@ -125,21 +165,29 @@ var FormWizard = function () {
                 $("#eoi-form-wizard").bootstrapWizard({
                     nextSelector: ".button-next",
                     previousSelector: ".button-previous",
-                    onTabClick: function (e, r, t, i) {
+                    onTabClick: function (tab, navigation, index) {
                         return false;
                     },
-                    onNext: function (e, a, n) {
-                        return i.hide(), t.hide(), 0 == r.valid() ? false : void o(e, a, n)
+                    onNext: function (tab, navigation, index) {
+                        passPanel.hide();
+                        failPanel.hide();
+
+                        if(!form.valid())
+                            return false;
+                        
+                        updateStep(tab, navigation, index);
                     },
-                    onPrevious: function (e, r, a) {
-                        i.hide(), t.hide(), o(e, r, a)
+                    onPrevious: function (tab, navigation, index) {
+                        passPanel.hide();
+                        failPanel.hide();
+                        updateStep(tab, navigation, index);
                     },
-                    onTabShow: function (e, r, t) {
-                        var i = r.find("li").length,
-                            a = t + 1,
-                            o = a / i * 100;
+                    onTabShow: function (tab, navigation, index) {
+                        var tabCount = navigation.find("li").length,
+                            current = index + 1,
+                            perc = current / tabCount * 100;
                         $("#eoi-form-wizard").find(".progress-bar").css({
-                            width: o + "%"
+                            width: perc + "%"
                         })
                     }
                 });
@@ -147,10 +195,12 @@ var FormWizard = function () {
                 $("#eoi-form-wizard").find(".button-previous").hide();
 
                 $("#eoi-form-wizard .button-submit").click(function () {
-                    alert("Finished! Hope you like it :)");
+                    if(form.valid()) {
+                        form.submit();
+                    }
                 }).hide();
 
-                $("#provide_primary").change(function () {
+                $("input[name=provide_primary]").change(function () {
                     if (this.checked) {
                         $('#primary_contact').removeClass('hide');
                     } else {
@@ -158,7 +208,7 @@ var FormWizard = function () {
                     }
                 });
 
-                $("#provide_secondary").change(function () {
+                $("input[name=provide_secondary]").change(function () {
                     if (this.checked) {
                         $('#secondary_contact').removeClass('hide');
                     } else {
@@ -166,7 +216,7 @@ var FormWizard = function () {
                     }
                 });
 
-                $('#eoi-form-wizard input[name=other_ella]').on('change', function () {
+                $('input[name=other_ella]').change(function () {
                     var val = $('input[name=other_ella]:checked', '#eoi-form-wizard').val();
                     if (val == 'yes') {
                         $('#ella-details').removeClass('hide');
@@ -180,10 +230,30 @@ var FormWizard = function () {
                 // })
 
                 // $('#eoi-form-wizard').bootstrapWizard('show', 3);
+                
             }
         }
     }
 } ();
+
+//http://stackoverflow.com/questions/1184624/convert-form-data-to-javascript-object-with-jquery
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 jQuery(document).ready(function () {
     FormWizard.init()
 });
