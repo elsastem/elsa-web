8272294c8614143fca952ed5c0465e7bb6aa7de5
Shane <shane.seaton@gmail.com>
2017-04-10 23:04:36 +1000

    WIP: trying to hook up lambda

--- eoi-site/js/contact_me.js
+++ eoi-site/js/contact_me.js
@@ -84,10 +84,11 @@ var SubscribeForm = function () {
             $("#signup-form-subscribe").click(function(e) {
                 e.preventDefault();
 
-                var url = "http://api.elsa.edu.au/web/subscribe"; //mailchimp
+                var url = "https://api.elsa.edu.au/web/subscribe"; //mailchimp
                 var apiKey = "huwyil4DTkGbPxduj8062871TorMtjM3CaSRS5Kh";
                 var formData = $("#signup-form").serializeObject();
-                formData.listId = '45f25b63ab';
+                //formData.listId = '45f25b63ab';
+                formData.email = formData.EMAIL;
 
                 $.ajax({
                     url: url,
