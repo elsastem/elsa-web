c59e2062f8c299089c069293b03584dbeb243bad
Shane <shane.seaton@gmail.com>
2017-07-17 16:42:51 +1000

    Move datatable files to local version from NPM

--- eoi-site/gulpfile.js
+++ eoi-site/gulpfile.js
@@ -12,7 +12,7 @@ var pkg = require('./package.json');
 var fs = require('fs');
 var markdown = require('nunjucks-markdown');
 var marked = require('marked');
-const csv=require('csvtojson');
+const csv = require('csvtojson');
 
 var BUILD_DIR = "./build"
 
@@ -26,44 +26,44 @@ var banner = ['/*!\n',
 ].join('');
 
 marked.setOptions({
-  renderer: new marked.Renderer(),
-  gfm: true,
-  tables: true,
-  breaks: false,
-  pendantic: false,
-  sanitize: false,
-  smartLists: true,
-  smartypants: false
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: false,
+    pendantic: false,
+    sanitize: false,
+    smartLists: true,
+    smartypants: false
 });
 
-gulp.task('nunjucks', function() {
+gulp.task('nunjucks', function () {
     return gulp.src('pages/**/*.+(html|nunjucks)')
         // Adding data to Nunjucks
-        .pipe(data(function(srcFile, cb) {
+        .pipe(data(function (srcFile, cb) {
             var mainData = JSON.parse(fs.readFileSync('./data.json', 'utf8'));
             var pilotData = JSON.parse(fs.readFileSync('./data-pilot.json', 'utf8'));
             // var schoolData = JSON.parse(fs.readFileSync('./data-schools.csv', 'utf8'));
             var schoolData = [];
             csv().fromFile('./data-schools.csv')
-            .on('json', (row) => {
-                schoolData.push(row);
-            })
-            .on('end', () => {
-                var result = Object.assign({}, mainData, pilotData, {schoolData});
-                cb(null, result);
-            })
+                .on('json', (row) => {
+                    schoolData.push(row);
+                })
+                .on('end', () => {
+                    var result = Object.assign({}, mainData, pilotData, { schoolData });
+                    cb(null, result);
+                })
         }))
         .pipe(nunjucksRender({
-             path: ['templates'],
-             manageEnv: function(env) {
+            path: ['templates'],
+            manageEnv: function (env) {
                 markdown.register(env, marked);
-             }
+            }
         }))
         .pipe(gulp.dest(BUILD_DIR))
 });
 
 // Compile LESS files from /less into /css
-gulp.task('less', function() {
+gulp.task('less', function () {
     return gulp.src(['less/agency.less', 'less/unomomento.less'])
         .pipe(less())
         .pipe(header(banner, { pkg: pkg }))
@@ -74,7 +74,7 @@ gulp.task('less', function() {
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', ['less'], function() {
+gulp.task('minify-css', ['less'], function () {
     return gulp.src([`${BUILD_DIR}/css/*.css`, `!${BUILD_DIR}/css/*.min.css`])
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
@@ -85,7 +85,7 @@ gulp.task('minify-css', ['less'], function() {
 });
 
 // Minify JS
-gulp.task('minify-js', function() {        
+gulp.task('minify-js', function () {
     return gulp.src('js/*.js')
         .pipe(uglify())
         .pipe(header(banner, { pkg: pkg }))
@@ -96,13 +96,13 @@ gulp.task('minify-js', function() {
         }))
 });
 
-gulp.task('copy-js', function() {
+gulp.task('copy-js', function () {
     gulp.src(['js/*.js'])
         .pipe(gulp.dest(`${BUILD_DIR}/js`))
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
+gulp.task('copy', function () {
     gulp.src(['img/**/*', '!img/photos/elsa/unused/**/*'])
         .pipe(gulp.dest(`${BUILD_DIR}/img`))
 
@@ -121,14 +121,19 @@ gulp.task('copy', function() {
     gulp.src(['node_modules/select2/dist/**'])
         .pipe(gulp.dest(`${BUILD_DIR}/vendor/select2`))
 
+    gulp.src(['node_modules/datatables.net/js/jquery.dataTables.js',
+        'node_modules/datatables.net-bs/js/dataTables.bootstrap.js',
+        'node_modules/datatables.net-bs/css/dataTables.bootstrap.css'])
+        .pipe(gulp.dest(`${BUILD_DIR}/vendor/datatables`))
+
     gulp.src([
-            'node_modules/font-awesome/**',
-            '!node_modules/font-awesome/**/*.map',
-            '!node_modules/font-awesome/.npmignore',
-            '!node_modules/font-awesome/*.txt',
-            '!node_modules/font-awesome/*.md',
-            '!node_modules/font-awesome/*.json'
-        ])
+        'node_modules/font-awesome/**',
+        '!node_modules/font-awesome/**/*.map',
+        '!node_modules/font-awesome/.npmignore',
+        '!node_modules/font-awesome/*.txt',
+        '!node_modules/font-awesome/*.md',
+        '!node_modules/font-awesome/*.json'
+    ])
         .pipe(gulp.dest(`${BUILD_DIR}/vendor/font-awesome`))
 })
 
@@ -136,7 +141,7 @@ gulp.task('copy', function() {
 gulp.task('default', ['nunjucks', 'less', 'minify-css', 'copy-js', 'minify-js', 'copy']);
 
 // Configure the browserSync task
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function () {
     browserSync.init({
         server: {
             baseDir: BUILD_DIR,
@@ -146,13 +151,13 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['copy', 'browserSync', 'nunjucks', 'less', 'copy-js', 'minify-css', 'minify-js'], function() {
+gulp.task('dev', ['copy', 'browserSync', 'nunjucks', 'less', 'copy-js', 'minify-css', 'minify-js'], function () {
     gulp.watch('data*', ['nunjucks']);
     gulp.watch('less/*.less', ['less', 'minify-css']);
     gulp.watch('js/*.js', ['copy-js', 'minify-js']);
     gulp.watch('pages/**/*.+(html|nunjucks)', ['nunjucks'])
     gulp.watch('templates/**/*.nunjucks', ['nunjucks'])
     // Reloads the browser whenever HTML or JS files change
-    gulp.watch( BUILD_DIR + '/*.html', browserSync.reload);
-    gulp.watch( BUILD_DIR + '/js/**/*.js', browserSync.reload);
+    gulp.watch(BUILD_DIR + '/*.html', browserSync.reload);
+    gulp.watch(BUILD_DIR + '/js/**/*.js', browserSync.reload);
 });
