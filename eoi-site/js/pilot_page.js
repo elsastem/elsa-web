88b2c5b8bc20d07540fb58a0aa02e11b419da3fa
Shane <shane.seaton@gmail.com>
2017-07-31 16:39:53 +1000

    Cori's latest content changes for the pilot/preschools release

--- eoi-site/js/pilot_page.js
+++ eoi-site/js/pilot_page.js
@@ -1,7 +1,8 @@
 jQuery(document).ready(function () {
     $('#schoolTable').dataTable({
         paging: false,
-        info: false
+        info: false,
+        order: [[ 2, 'asc' ], [ 1, 'asc' ]]
     });
 });
 
